8d41fac5bcfaec0828bd10d44e2d8e68c408af68
abitaaugustine <32564955+abitaaugustine@users.noreply.github.com>
2019-04-18 19:50:05 +0530

    Add files via upload

---
+++ Network Lab/exp 5/interprocess.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+ class PipeExample {
+
+    public static void main(String[] args) throws IOException {
+
+        final PipedOutputStream output = new PipedOutputStream();
+        final PipedInputStream  input  = new PipedInputStream(output);
+
+
+        Thread thread1 = new Thread(new Runnable() {
+          
+            public void run() {
+                try {
+                    output.write("Hello world, pipe!".getBytes());
+                } catch (IOException e) {
+                }
+            }
+        });
+
+
+        Thread thread2 = new Thread(new Runnable() {
+            
+            public void run() {
+                try {
+                    int data = input.read();
+                    while(data != -1){
+                        System.out.print((char) data);
+                        data = input.read();
+                    }
+                } catch (IOException e) {
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+    }
+}
+
